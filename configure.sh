ad94840b3b591a6dafba9f022f87dd9a012cb42c
Viktor <viktor@axonnet.hu>
2017-02-18 00:27:36 +0100

    Adding configure.sh to .travis.yml

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Configuration script for DokuWiki Platform
 
+echo Configuring DokuWiki Platform
+
 ln -sv lib/tpl/bootsrap3     dokuwiki/lib/tpl/
 ln -sv lib/plugins/include   dokuwiki/lib/plugins/
