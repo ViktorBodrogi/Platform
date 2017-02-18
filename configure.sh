4eb07df9709a523d81e7bfbff936e833116e7580
Bodrogi Viktor <viktor@axonnet.hu>
2017-02-18 10:45:37 +0100

    * Update .travis.yml
    * Using configure.sh
    
    * Using relative links
    
    * Do not test core dokuwiki, only plugins
    * Add slack notification
    * Add extension=sqlite.so
    * Add env: DOKUWIKI=master/stable
    * See #20

--- configure.sh
+++ configure.sh
@@ -3,6 +3,6 @@
 cp -v dokuwiki/_test/mysql.conf.php.dist        dokuwiki/_test/mysql.conf.php
 cp -v dokuwiki/_test/pgsql.conf.php.dist        dokuwiki/_test/pgsql.conf.php
 
-ln -sv dokuwiki-plugins/lib/plugins/include     dokuwiki/lib/plugins/
-ln -sv dokuwiki-plugins/lib/plugins/sqlite      dokuwiki/lib/plugins/
-ln -sv dokuwiki-plugins/lib/plugins/data        dokuwiki/lib/plugins/
+ln -sv ../../../dokuwiki-plugins/lib/plugins/include     dokuwiki/lib/plugins/
+ln -sv ../../../dokuwiki-plugins/lib/plugins/sqlite      dokuwiki/lib/plugins/
+ln -sv ../../../dokuwiki-plugins/lib/plugins/data        dokuwiki/lib/plugins/
