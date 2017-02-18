152ea44625ddd5e8227ef72c65acb0c567b3974a
Bodrogi Viktor <viktor@axonnet.hu>
2017-02-18 04:14:38 +0100

    Update configure.sh (#24)
    See #13

--- configure.sh
+++ configure.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # Configuration script for DokuWiki Platform
 
-echo Configuring DokuWiki Platform
+echo === Configuring DokuWiki Platform ===
+LIB=lib
+TPL=$LIB/tpl
+PLUGINS=$LIB/plugins
 
-ln -sv lib/tpl/bootsrap3        dokuwiki/lib/tpl/
+DOKUWIKI=dokuwiki
+DOKUWIKI_TPL=$DOKUWIKI/lib/tpl
+DOKUWIKI_PLUGINS=$DOKUWIKI/lib/plugins
 
-ln -sv lib/plugins/data         dokuwiki/lib/plugins/
-ln -sv lib/plugins/include      dokuwiki/lib/plugins/
-ln -sv lib/plugins/sqlite       dokuwiki/lib/plugins/
+ln -sv $TPL/bootsrap3           $DOKUWIKI_TPL/
+
+ln -sv $PLUGINS/data            $DOKUWIKI_PLUGINS/
+ln -sv $PLUGINS/include         $DOKUWIKI_PLUGINS/
+ln -sv $PLUGINS/sqlite          $DOKUWIKI_PLUGINS/
