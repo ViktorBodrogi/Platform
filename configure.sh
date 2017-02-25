16c4a218869d98a4a40d916e2653241c5409d80b
Bodrogi Viktor <viktor@axonnet.hu>
2017-02-25 06:52:34 +0100

    Platform 0.1 (#52)
    * Submodule dokuwiki #2
    * Added bootstrap3 #8
    * Submodule dokuwiki-plugins #6
      * include plugin #7
      * sqlite plugin #20 #21
      * data plugin, see #16 #19
    
    * Create configure.sh #14 #13
      * Update configure.sh #24
      * Configuration script for DokuWiki Platform
      * See #13
    
    * Travis config #4 #9 #12
      * Create and update .travis.yml
      * PHP versions
      * Setup Travis/Slack notification
      * Added Travis status images.
      * PHP versions
      * Disable PHP 5.6 and 7.0 builds
      * Disable hhvm build
      * Adding configure.sh to .travis.yml
      * Running tests for plugins group
    
    * Update README.md
      * Added Travis status images.
    
    * Platform-0.1 #5
    * Plugins-0.1 #43
    * Docker #49
    
    Author:    Bodrogi Viktor <viktor@axonnet.hu>
    Changes to be committed:
    * new file:   .gitmodules
    * new file:   .travis.yml
    * new file:   Dockerfile
    * modified:   README.md
    * new file:   configure.sh
    * new file:   dokuwiki
    * new file:   dokuwiki-plugins
    * new file:   lib/plugins/data
    * new file:   lib/plugins/include
    * new file:   lib/plugins/sqlite
    * new file:   lib/tpl/bootstrap3

---
+++ configure.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Configuration script for DokuWiki Platform
+
+echo === Configuring DokuWiki Platform ===
+
+cp -v dokuwiki/_test/mysql.conf.php.dist        dokuwiki/_test/mysql.conf.php
+cp -v dokuwiki/_test/pgsql.conf.php.dist        dokuwiki/_test/pgsql.conf.php
+
+ln -sv ../../../dokuwiki-plugins/lib/plugins/include     dokuwiki/lib/plugins/
+ln -sv ../../../dokuwiki-plugins/lib/plugins/sqlite      dokuwiki/lib/plugins/
+ln -sv ../../../dokuwiki-plugins/lib/plugins/data        dokuwiki/lib/plugins/
+
+# BUG #34
+rm -fr dokuwiki/lib/plugins/include/lang/ar
