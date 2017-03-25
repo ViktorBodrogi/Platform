4c2b01d6dff84c79401b2a9e46d266b7f01c30a0
Bodrogi Viktor <viktor@axonnet.hu>
2017-03-25 13:16:02 +0100

    - Updating to latest dokuwiki-plugins.

--- configure.sh
+++ configure.sh
@@ -1,14 +1,40 @@
 #!/bin/sh
 # Configuration script for DokuWiki Platform
 
-echo === Configuring DokuWiki Platform ===
+echo === Configuring DokuWiki ===
 
 cp -v dokuwiki/_test/mysql.conf.php.dist        dokuwiki/_test/mysql.conf.php
 cp -v dokuwiki/_test/pgsql.conf.php.dist        dokuwiki/_test/pgsql.conf.php
 
-ln -sv ../../../dokuwiki-plugins/lib/plugins/include     dokuwiki/lib/plugins/
-ln -sv ../../../dokuwiki-plugins/lib/plugins/sqlite      dokuwiki/lib/plugins/
-ln -sv ../../../dokuwiki-plugins/lib/plugins/data        dokuwiki/lib/plugins/
 
-# BUG #34
+echo === Copy/link Plugins ===
+
+plugins=dokuwiki/lib/plugins
+
+function plug {
+    echo - $1
+    if [ ! -e $plugins/$1 ]; then
+        ln -sv ../../../dokuwiki-plugins/lib/plugins/$1 $plugins/;
+    fi
+}
+
+plug advanced
+plug bootswrapper
+plug data
+plug iframe
+plug include
+plug indexmenu
+plug meta
+plug pagelist
+plug pagetemplater
+plug sqlite
+plug tabinclude
+plug tag
+plug templatepagename
+plug wrap
+
+
+echo === Fixes ===
+
+echo - Fix BUG '#34'
 rm -fr dokuwiki/lib/plugins/include/lang/ar
